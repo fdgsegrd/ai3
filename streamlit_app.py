0fa3c408ae111524736c3273e5fd3c98ff54c2af
fdgsegrd <h098s02605@chunchon.gwe.hs.kr>
2025-11-18 12:35:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,11 +70,11 @@ st.markdown("---")
 CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
 
      labels[0]: {
-       "texts": ["우루과이", "골키퍼", "신의손"],
+       "texts": ["브라지", "골키퍼", "신의손"],
        "images": ["https://i.namu.wiki/i/-igH7T_rzORwxplNdDV1O712u51xRLQGUQaw3DfnZFxITvkRbSjYFKZ99-OAfsA8oE6VZvy0AyUPXikfsWq7e6IQdq-qO6LRORpGZpTu6P3-7MjbSKKmBwI9T-ctB9-qCTRIB_L-Ojfk3kqgOfoVRQ.webp", "https://.../jjampong2.jpg"],
        "videos": ["https://www.youtube.com/watch?v=aRaVvrDaBKI"]
      }, labels[1]: {
-       "texts": ["우루과이", "골키퍼", "신의손"],
+       "texts": ["포르투갈", "날강두", "멘유"],
        "images": ["https://i.namu.wiki/i/-igH7T_rzORwxplNdDV1O712u51xRLQGUQaw3DfnZFxITvkRbSjYFKZ99-OAfsA8oE6VZvy0AyUPXikfsWq7e6IQdq-qO6LRORpGZpTu6P3-7MjbSKKmBwI9T-ctB9-qCTRIB_L-Ojfk3kqgOfoVRQ.webp", "https://.../jjampong2.jpg"],
        "videos": ["https://www.youtube.com/watch?v=aRaVvrDaBKI"]
      },labels[2]: {
